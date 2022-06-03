595522c36b33a514aec40ccb820cd1e1715e2215
Webbie <webdevelopercs976@gmail.com>
2022-06-02 21:10:53 -0400

    learned about states

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
+
 const Home = () => {
+    const [name, setName] = useState('Mula');
+    const [age, setAge] = useState(20)
+
+    const handleClick = () => {
+        setName('Popo')
+        setAge('78')
+    }
+
     return ( 
         <div className="home">
             <h2>Home</h2>
+            <p> { name } is {age} years old</p>
+            <button onClick={handleClick}>Click me</button>
         </div>
      );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,16 +4,10 @@ const Navbar = () => {
             <h1>Welcome To KidsLearn</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create" style={{
-                    color: "white",
-                    backgroundColor: '#f1356d',
-                    borderRadius: '8px'
-                }}>New Post</a>
+                <a href="/create">New Post</a>
             </div>
         </nav>
     );
 }
  
 export default Navbar;
-
-
