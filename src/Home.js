05a4aaa0f65dc53ee5abaee734c3c54bfeec26bf
Webbie <webdevelopercs976@gmail.com>
2022-06-02 21:20:36 -0400

    outputting an array of elements using map

--- src/Home.js
+++ src/Home.js
@@ -2,19 +2,20 @@ import { useState } from "react";
 
 
 const Home = () => {
-    const [name, setName] = useState('Mula');
-    const [age, setAge] = useState(20)
-
-    const handleClick = () => {
-        setName('Popo')
-        setAge('78')
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
 
     return ( 
         <div className="home">
-            <h2>Home</h2>
-            <p> { name } is {age} years old</p>
-            <button onClick={handleClick}>Click me</button>
+          {blogs.map((blog) => (
+              <div className="blog-preview" key={blog.id}>
+                  <h2>{ blog.title }</h2>
+                  <p>Written by { blog.author }</p>
+              </div>
+          ))}
         </div>
      );
 }
